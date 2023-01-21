3313f078768b7b116a1cce96dd2b7c0558f6e49c
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-21 14:13:45 +0900

    initial commit

---
+++ Assets/UdpController/Scripts/Io/InputEventManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+namespace UdpController
+{
+    public class InputEventManager : MonoBehaviour
+    {
+        public void OnSubmit(InputAction.CallbackContext context)
+        {
+            Debug.Log("Hello!")
+        }
+    }
+}
